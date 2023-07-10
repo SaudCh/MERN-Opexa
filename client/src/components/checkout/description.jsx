a710ba3b35e4d26ed241ba0e9d78acabef48f1c6
unknown <SP19-BCS-057@cuilahore.edu.pk>
2023-07-10 13:38:50 +0500

    main

--- client/src/components/checkout/description.jsx
+++ client/src/components/checkout/description.jsx
@@ -2,6 +2,28 @@ import React from "react";
 
 import { MdDone } from "react-icons/md";
 function Description() {
+  const descrip = [
+    {
+      id: 1,
+      text: " Adjustable Bracelet..",
+    },
+    {
+      id: 2,
+      text: "crystal face.",
+    },
+    {
+      id: 3,
+      text: "Quartz movement.",
+    },
+    {
+      id: 4,
+      text: "Stainless steel/leather.",
+    },
+    {
+      id: 5,
+      text: "Day and date function.",
+    },
+  ];
   return (
     <div className="my-5 md:mx-16 mx-4">
       <div className="grid grid-cols-1 md:grid-cols-3">
@@ -31,31 +53,14 @@ function Description() {
             woven accessory bands. Ut elit tellus, luctus nec ullamcorper
             mattis, pulvinar dapibus leo.
           </p>
-
-          <span className="flex flex-row text-gray-600 text-sm font-thin items-center mb-3 mt-8">
-            <MdDone className="text-gray-800 text-xl mr-3 font-bold" />{" "}
-            Adjustable Bracelet..
-          </span>
-
-          <span className="flex flex-row text-gray-600 text-sm font-thin items-center mb-3">
-            <MdDone className="text-gray-800 text-xl mr-3 font-bold" /> Mineral
-            crystal face.
-          </span>
-
-          <span className="flex flex-row text-gray-600 text-sm font-thin items-center mb-3">
-            <MdDone className="text-gray-800 text-xl mr-3 font-bold" /> Mineral
-            Quartz movement.
-          </span>
-
-          <span className="flex flex-row text-gray-600 text-sm font-thin items-center mb-3">
-            <MdDone className="text-gray-800 text-xl mr-3 font-bold" /> Mineral
-            Stainless steel/leather.
-          </span>
-
-          <span className="flex flex-row text-gray-600 text-sm font-thin items-center mb-3">
-            <MdDone className="text-gray-800 text-xl mr-3 font-bold" /> Mineral
-            Day and date function.
-          </span>
+          {descrip.map((des) => {
+            return (
+              <span className="flex flex-row text-gray-600 text-sm font-thin items-center my-5">
+                <MdDone className="text-gray-800 text-xl mr-3 font-bold" />{" "}
+                {des.text}
+              </span>
+            );
+          })}
         </div>
         <div>
           <div className="bg-gray-50 flex items-center justify-center ">
