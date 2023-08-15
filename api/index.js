92d16ffc0b0401dbd8b8d2d1e3ba8a4b97dd693c
Saud ul Hassan <56765274+SaudCh@users.noreply.github.com>
2023-08-15 22:41:56 +0500

    15-Aug-2

--- api/index.js
+++ api/index.js
@@ -34,6 +34,9 @@ const corsOptions = {
 
 app.use(cors(corsOptions))
 
+const { stripeHook } = require('./controllers/transcationController')
+app.post('/stripe-hook', stripeHook)
+
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
